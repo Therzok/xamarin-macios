96d92ca7c58e7467bdb0de541186a1dfc8346aa8
Marius Ungureanu <therzok@gmail.com>
2019-12-08 00:37:52 +0200

    ArgumentNullException allows passing null as message parameter

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -1705,10 +1705,7 @@ namespace ObjCRuntime {
 		internal static T ThrowOnNull<T> (T obj, string name, string message = null) where T : class
 		{
 			if (obj == null) {
-				if (message == null)
-					throw new ArgumentNullException (name);
-				else
-					throw new ArgumentNullException (name, message);
+				throw new ArgumentNullException (name, message);
 			}
 			return obj;
 		}
